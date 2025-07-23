348413c4fc775b6ff6712628ef6aa9a02f706c24
tyydev_ <noobastrox@gmail.com>
2025-07-23 17:12:21 +0700

    Finished project

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from stats import *
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text(path_to_books):
     with open(path_to_books) as file:
@@ -6,9 +11,9 @@ def get_book_text(path_to_books):
     return book_text
 
 def main():
-
+    
     # Initializing the variables
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     num_words = get_num_words(book_text)
     num_char = get_num_char(book_text)
@@ -19,7 +24,7 @@ def main():
         print(f"{item['char']}: {item['num']}")
     
     # Watermark (REQUIRED BY HR)
-    print("----------- END -----------\nThank you for using BookBot by tyydev1!")
-    
+    print("----------- END -----------\nThank you for using BookBot by tyydev1!\n\nby aldi")
+
 if __name__ == "__main__":
     main()
